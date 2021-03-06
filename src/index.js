c8b0a1c45a0a02bc0de17cfb9294fdd1d3dc5b44
Remy Oudemans <remyoudemans@gmail.com>
2021-03-06 23:44:34 +0000

    feat: make it into more of a game

--- src/index.js
+++ src/index.js
@@ -29,18 +29,64 @@ let app = new Application({
 
 document.body.appendChild(app.view);
 
+let cat, state, cookie, score, scoreVal;
+
+const positionRandomly = sprite => {
+  sprite.position.set(
+    Math.random() * (STAGE_WIDTH - sprite.width) + sprite.width / 2,
+    Math.random() * (STAGE_HEIGHT - sprite.height) + sprite.height / 2
+  );
+};
+
 loader
-  .add("images/cat.png")
+  .add([
+    { name: 'cat', url:"images/cat.png" },
+    { name: 'cookie', url: "images/cookie.png" }
+  ])
   .load(setup);
+
+const play = delta => {
+  if (cat.vx > 0 && cat.x >= STAGE_WIDTH - cat.width / 2 || cat.vx < 0 && cat.x <= cat.width / 2) {
+    cat.vx = 0;
+  }
+
+  if (cat.vy < 0 && cat.y <= cat.height / 2 || cat.vy > 0 && cat.y >= STAGE_HEIGHT - cat.height / 2) {
+    cat.vy = 0;
+  }
+
+  if (Math.abs(cat.x - cookie.x) < (cat.width + cookie.width) / 2 && Math.abs(cat.y - cookie.y) < (cat.height + cookie.height) / 2) {
+    positionRandomly(cookie);
+
+    scoreVal += 20;
+  }
+
+  cat.x += cat.vx;
+  cat.y += cat.vy;
+  cat.rotation += cat.vrotation;
+  score.text = `Score: ${scoreVal}`;
+}
+
+const gameLoop = (delta) => {
+  state(delta);
+};
 
 // This `setup` function will run when the image has loaded
 function setup(_, resources) {
 
   // Create the cat sprite
-  const cat = new Sprite(resources["images/cat.png"].texture)
+  cat = new Sprite(resources.cat.texture);
+  cookie = new Sprite(resources.cookie.texture);
+  scoreVal = 0
+  score = new PIXI.Text(`Score: ${scoreVal}`,{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
   
   // Add the cat to the stage
   app.stage.addChild(cat);
+  app.stage.addChild(cookie);
+  app.stage.addChild(score);
+
+  cookie.scale.set(0.1, 0.1);
+  cookie.anchor.set(0.5, 0.5);
+  cookie.position.set(70, 70);
 
   cat.anchor.x = 0.5;
   cat.anchor.y = 0.5;
@@ -53,19 +99,7 @@ function setup(_, resources) {
   cat.vy = 0;
   cat.vrotation = 0;
 
-  const gameLoop = () => {
-    if (cat.vx > 0 && cat.x >= STAGE_WIDTH - cat.width / 2 || cat.vx < 0 && cat.x <= cat.width / 2) {
-      cat.vx = 0;
-    }
-
-    if (cat.vy < 0 && cat.y <= cat.height / 2 || cat.vy > 0 && cat.y >= STAGE_HEIGHT - cat.height / 2) {
-      cat.vy = 0;
-    }
-
-    cat.x += cat.vx;
-    cat.y += cat.vy;
-    cat.rotation += cat.vrotation;
-  };
+  state = play;
 
   app.ticker.add(gameLoop);
 
@@ -79,29 +113,27 @@ function setup(_, resources) {
   });
 
   document.addEventListener('keydown', (e) => {
-    if (e.key === 'ArrowUp') {
-      cat.vx = 0;
-      cat.vy = -MOTION_SPEED;
-    }
-
-    if (e.key === 'ArrowDown') {
-      cat.vx = 0;
-      cat.vy = MOTION_SPEED;
-    }
-
-    if (e.key === 'ArrowLeft') {
-      cat.vx = -MOTION_SPEED;
-      cat.vy = 0;
-    }
-
-    if (e.key === 'ArrowRight') {
-      cat.vx = MOTION_SPEED;
-      cat.vy = 0;
-    }
-
-    if (e.key === ' ') {
-      cat.vx = 0;
-      cat.vy = 0;
+    switch(e.key) {
+      case 'ArrowUp':
+        cat.vx = 0;
+        cat.vy = -MOTION_SPEED;
+        break;
+      case 'ArrowDown':
+        cat.vx = 0;
+        cat.vy = MOTION_SPEED;
+        break;
+      case 'ArrowLeft':
+        cat.vx = -MOTION_SPEED;
+        cat.vy = 0;
+        break;
+      case 'ArrowRight':
+        cat.vx = MOTION_SPEED;
+        cat.vy = 0;
+        break;
+      case ' ':
+        cat.vx = 0;
+        cat.vy = 0;
+        break;
     }
   })
 }
