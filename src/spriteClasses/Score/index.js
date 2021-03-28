5d24e4ba857bf405f30ee74f0ad4f035e09b0b5c
Remy Oudemans <remyoudemans@gmail.com>
2021-03-28 19:12:55 +0100

    feat: add black background behind score

--- src/spriteClasses/Score/index.js
+++ src/spriteClasses/Score/index.js
@@ -1,17 +1,28 @@
-import { Text } from 'pixi.js';
+import { Text, Graphics } from 'pixi.js';
 
 export default class Score {
   constructor(app) {
     this.innerValue = 0;
     this.app = app;
 
+    this.fontSize = 24;
+    this.lineHeight = 30;
+
+    const graphics = new Graphics()
+      .beginFill()
+      .drawRect(0, 0, this.app.view.width, this.lineHeight)
+      .endFill();
+
+    this.app.stage.addChild(graphics);
+
     this.sprite = new Text(
       this.text,
       {
         fontFamily : 'Arial',
-        fontSize: 24,
+        fontSize: this.fontSize,
+        lineHeight: this.lineHeight,
         fill : 0xff1010,
-        align : 'center'
+        align : 'center',
       }
     );
   }
